3a0b23f2b87a3aa5519e125a4363e816394cf1fa
Dimitris Mitsios <di.mitsios@gmail.com>
2025-08-27 22:00:24 +0300

    cleaned unused imports in host/main.rs

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,36 +1,10 @@
-use clap::Parser;
 use eyre::Result;
-use openvm::platform::print;
 use openvm_build::GuestOptions;
-use std::path::{self, Path, PathBuf};
-use std::{fs, env, sync::Arc};
-use openvm::platform::memory::MEM_SIZE;
-use openvm_benchmarks_prove::util::BenchmarkCli;
-use openvm_circuit::arch::instructions::exe::VmExe;
-use openvm_rv32im_circuit::Rv32ImConfig;
-use openvm_rv32im_transpiler::{
-    Rv32ITranspilerExtension, Rv32IoTranspilerExtension, Rv32MTranspilerExtension,
-};
-use openvm_sdk::{config::{SdkVmConfig, AppConfig}, StdIn, Sdk, F};
-use openvm_circuit::openvm_stark_sdk::{bench::run_with_metric_collection, p3_baby_bear::BabyBear, config::FriParameters};
-use openvm_transpiler::{transpiler::Transpiler, FromElf, elf::Elf};
-
-use ream_consensus::{attestation::{self, Attestation}, electra::beacon_state::BeaconState};
-
-// Dependencies for testing deserialization
-use std::{
-    collections::{HashMap, VecDeque},
-};
-use serde::{Deserialize, Serialize};
-use openvm_stark_backend::p3_field::FieldAlgebra;
-use p3_monty_31::MontyField31;
-use p3_baby_bear::BabyBearParameters;
+use std::path::{PathBuf};
+use std::{env, sync::Arc};
+use openvm_sdk::{config::{SdkVmConfig, AppConfig}, StdIn, Sdk};
+use openvm_circuit::openvm_stark_sdk::{config::FriParameters};
 use ream_lib::file::ssz_from_file;
-fn read_elf() -> Result <(), Box<dyn std::error::Error>> {
-    let elf_bytes = fs::read("../guest")?;
-    let elf = Elf::decode(&elf_bytes, MEM_SIZE as u32)?;
-    Ok(())
-}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -68,23 +42,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let attestation_path: &PathBuf = &std::path::PathBuf::from(env::var("CARGO_MANIFEST_DIR")?)
             .join("../assets/one_basic_attestation/attestation.ssz_snappy");
 
-    let pre_state: BeaconState = utils::read_file(
-        &std::path::PathBuf::from(env::var("CARGO_MANIFEST_DIR")?)
-            .join("../assets/one_basic_attestation/pre.ssz_snappy"),
-    );
-    let attestation: Attestation = utils::read_file(
-        &std::path::PathBuf::from(env::var("CARGO_MANIFEST_DIR")?)
-            .join("../assets/one_basic_attestation/attestation.ssz_snappy"),
-    );
-
     let mut stdin = StdIn::default();
-    // let pre_state_words = openvm::serde::to_vec(&pre_state).unwrap();
     let pre_state_bytes: Vec<u8> =ssz_from_file(&pre_state_path); // pre_state_words.iter().flat_map(|w| w.to_le_bytes()).collect();
-    // let attestation_words = openvm::serde::to_vec(&attestation).unwrap();
     let attestation_bytes: Vec<u8> = ssz_from_file(&attestation_path); // attestation_words.iter().flat_map(|w| w.to_le_bytes()).collect();
     let pre_state_len_bytes: Vec<u8> = pre_state_bytes.len().to_be_bytes().to_vec();
-    // let pre_field_data: Vec<MontyField31<BabyBearParameters>> = pre_bytes.iter().map(|b| F::from_canonical_u8(*b)).collect();
-    // let stdin.push_back(pre_field_data);
 
     stdin.write_bytes(&pre_state_len_bytes);
     stdin.write_bytes(&pre_state_bytes);
@@ -114,15 +75,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Generating AppProvingKey -- END");
 
     // 9a. Generate a proof
-    println!("Generating proof -- START");
-    let proof = sdk.generate_app_proof(app_pk.clone(), app_committed_exe.clone(), stdin.clone())?;
-    println!("Generating proof -- END");
+    // println!("Generating proof -- START");
+    // let proof = sdk.generate_app_proof(app_pk.clone(), app_committed_exe.clone(), stdin.clone())?;
+    // println!("Generating proof -- END");
 
-    // 10. Verify your program
-    println!("Verifying proof -- START");
-    let app_vk = app_pk.get_app_vk();
-    sdk.verify_app_proof(&app_vk, &proof)?;
-    println!("Verifying proof -- END");
+    // // 10. Verify your program
+    // println!("Verifying proof -- START");
+    // let app_vk = app_pk.get_app_vk();
+    // sdk.verify_app_proof(&app_vk, &proof)?;
+    // println!("Verifying proof -- END");
 
     Ok(())
 }
